ab336c46a1635fe07f000eba63faf40bcf58ffba
123321 <75672444+Attack783@users.noreply.github.com>
2022-08-31 15:58:10 +0800

    Update main.py

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ user_id = os.environ["USER_ID"]
 # user_id_02 = 'oXQhR6WI_labGe7SUni7Bd_HXmq8'
 
 # template_id = os.environ["TEMPLATE_ID"] 	
-template_id = 'bdUnnK-Pkvpni3PndZVZ_oUaFHK2Obv9FL_m2bULkUQ'
+template_id = 'oGZ_EhC5r3SURvHSs9WlCR3pCWcznewu9mJ2zrAblWI'
 
 def get_weather():
   url = "http://autodev.openspeech.cn/csp/api/v2.1/weather?openId=aiuicus&clientType=android&sign=android&city=" + city
